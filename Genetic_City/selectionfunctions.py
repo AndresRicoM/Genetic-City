5af183153df6027281fd4bdc03422e834a4dd3ef
Andres Rico <32314497+AndresRicoM@users.noreply.github.com>
2019-08-16 13:43:42 -0500

    Delete selectionfunctions.py

--- Genetic_City/selectionfunctions.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-def match(participant_1, participant_2, participant_3, participant_4):
-    match_array = [participant_1, participant_2, participant_3, participant_4]
-    return np.argmax(match_array)
